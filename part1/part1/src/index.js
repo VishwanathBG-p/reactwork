bc05b80f334f13972480104f4c63c371be305355
vishwanathB <vishwanathbg90@gmail.com>
2021-01-22 21:16:49 +0530

    added

--- part1/part1/src/index.js
+++ part1/part1/src/index.js
@@ -1,17 +1,88 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+//display the helloworld using react
+/*const App=()=>(
+  <div>
+    <h1>Hello World</h1>
+  </div>
+)
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <App />,
+  document.getElementById('root')
+);*/
+
+//example1
+/*
+const App=()=>{
+  const now =new Date()
+  const a=10
+  const b=20
+
+  return(
+    <div>
+      <p>Hello world</p>
+      <p>total:{a+b} </p>
+      <p> Todays Date is {now.toString()}</p>
+    </div>
+
+  )
+
+}*/
+//example2 in JSX
+/*
+const App=()=>{
+  const name="vishwa1";
+  const age=58;
+
+  return(
+    //there Must be a parent tag
+    <>
+    <p>Hello </p>
+    <p>name:{name} age:{age}</p>
+    <p>name:"hello" age:"22"</p>
+    </>
+
+  )
+}
+*/
+const Hello = (props) => {
+  return (
+    <div>
+      <p>
+        Hello {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <div>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+    </div>
+  )
+}
+
+const App=()=>{
+  const corse='half Stack application deelopment'
+  const part1='Fundamentals Of React'
+  const exercises1=10
+  const part2="using the props to pass the data"
+  const exercises2=7
+  const part3="State of component"
+  const exercises3=14
+
+  
+}
+
+ReactDOM.render(<App/>,document.getElementById('root'))
+
+
+
